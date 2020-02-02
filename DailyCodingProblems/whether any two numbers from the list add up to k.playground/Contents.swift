c43927d1dcefe3b5c4f1cf8d61596ce28fba295f
pitt500 <pedro.rojaslo50@gmail.com>
2020-02-01 21:14:48 -0700

    Add problem description

--- DailyCodingProblems/whether any two numbers from the list add up to k.playground/Contents.swift
+++ DailyCodingProblems/whether any two numbers from the list add up to k.playground/Contents.swift
@@ -1,5 +1,15 @@
 import Foundation
 
+/**
+ This problem was recently asked by Google.
+
+ Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+
+ For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+ Bonus: Can you do this in one pass?
+ */
+
 func twoNumbersEqualToK(array: [Int], k: Int) -> Bool {
   let objectSet = Set(array)
   
@@ -14,5 +24,5 @@ func twoNumbersEqualToK(array: [Int], k: Int) -> Bool {
   return false
 }
 
-let result = isTheSumEqualToK(array: [10, 15, 3, 7], k: 1)
+let result = twoNumbersEqualToK(array: [10, 15, 3, 7], k: 1)
 print(result)
